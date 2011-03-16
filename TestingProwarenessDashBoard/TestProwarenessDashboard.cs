4125ab7d3ef2ff0f9bd9f34992d4dae989dc5bbc
unknown <Administrator@.isenseosl.local>
2011-03-16 15:41:59 +0530

    Added Test for testing the IP Cam URLs

--- TestingProwarenessDashBoard/TestProwarenessDashboard.cs
+++ TestingProwarenessDashBoard/TestProwarenessDashboard.cs
@@ -30,25 +30,43 @@ namespace TestingProwarenessDashBoard
         [TestMethod]
         public void TestToCheckTheQualityKpi()
         {
-            Team team = new Team("Calvi", 20, 0.5, 20);
+            Team team = new Team("Calvi", 20, 0.5, 20,"");
             Assert.AreEqual(team.quality, 20);
         }
 
         [TestMethod]
         public void TestToCheckTheVelocityKpi()
         {
-            Team team = new Team("Calvi", 20, 0.5, 20);
+            Team team = new Team("Calvi", 20, 0.5, 20,"");
             Assert.AreEqual(team.quality, 20);
         }
 
         [TestMethod]
         public void TestToCheckTheReliabilityKpi()
         {
-            Team team = new Team("Calvi", 20, 0.5, 20);
+            Team team = new Team("Calvi", 20, 0.5, 20,"");
             Assert.AreEqual(team.quality, 20);
         }
 
+        [TestMethod]
+        public void TestToCheckIfIndiaIpCameraWorks()
+        {
+            String IndiaIPCamURL = "http://192.168.1.201/view/viewer_index.shtml?id=5";
+            WebRequest webRequest = WebRequest.Create(IndiaIPCamURL);
+            WebResponse webResponse;
+            webResponse = webRequest.GetResponse();
+            Assert.IsNotNull(webResponse);
+        }
 
+        [TestMethod]
+        public void TestToCheckIfNLIpCameraWorks()
+        {
+            String NeitherLandIPCamURL = "http://192.168.0.30/view/viewer_index.shtml?id=11";
+            WebRequest webRequest = WebRequest.Create(NeitherLandIPCamURL);
+            WebResponse webResponse;
+            webResponse = webRequest.GetResponse();
+            Assert.IsNotNull(webResponse);
+        }
 
     }
     
